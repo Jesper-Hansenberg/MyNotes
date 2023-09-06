fb4f327551c714daf963e30cdf7d4feb921200c2
Jesper Madsen <jema@hansenberg.dk>
2023-09-06 08:53:13 +0200

    Gode ændringer

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Page FirstPage = new Page("Min første side i min awesome bog");
+
+Page second = new Page("Min anden side i min awesome bog");
+
+Page third = new Page("Min tredje side side i min awesome bog");
+
+Chapter Kapitel1 = new Chapter("Variabler");
+
+Kapitel1.Pages.Add(FirstPage);
+Kapitel1.Pages.Add(second);
+Kapitel1.Pages.Add(third);
+
+
+Kapitel1.Read();
+
+Console.ReadLine();
