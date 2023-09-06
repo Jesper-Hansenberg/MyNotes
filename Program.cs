b32ee1be58eb746e675f59d841cc84cedbb4d8f4
Jesper Madsen <jema@hansenberg.dk>
2023-09-06 09:26:56 +0200

    gode ændringer

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 Page second = new Page("Min anden side i min awesome bog");
 
 Page third = new Page("Min tredje side side i min awesome bog");
+Page foruth  = new Page("Min tredje side side i min awesome bog");
 
 Chapter Kapitel1 = new Chapter("Variabler");
 
