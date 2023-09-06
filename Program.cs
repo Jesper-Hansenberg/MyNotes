5fad57996030d9fe585135cf1772bd90e27f99fc
Jesper Madsen <jema@hansenberg.dk>
2023-09-06 11:15:30 +0200

    Godt besked til

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿Page p = new Page("hejsa");
+﻿Page myfirstpage = new Page("En klasse er noget man instantierere med NEW og så er det et object.");
+Page page2 = new Page("asdasdasd");
 
-Chapter chap1 = new Chapter("vars");
-
-chap1.Pages.Add(p);
-
-Book book = new Book("Jespers bog");
-
-book.Chapters.Add(chap1);
+List<Page> KapitelPages = new List<Page>(){
+    myfirstpage,
+    page2
+};
 
 
-book.Start();
+List<Chapter> chapters = new List<Chapter>();
+Chapter kapitel1 = new Chapter("haps", KapitelPages);
+chapters.Add(kapitel1);
+
+Book mybook = new Book("asdasd", chapters);
+
+
+mybook.Start();
